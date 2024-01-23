671c078b816b3a186eb57d2bb7c4f25934b6244d
dylanmcn <mcnamarad@uncw.edu>
2024-01-23 14:02:46 -0500

    Add files via upload

--- calculate_risk_premium.m
+++ calculate_risk_premium.m
@@ -11,9 +11,15 @@ function [X]=calculate_risk_premium(A,M,X,MMT,OF);
 a = 365;
 b = 200;
 c = 5;
-d = 0.1;
-e = 0.04;
+d = 0.01;
+e = 0.5*0.04;
 f = 0.02;
+return_interval = 0.05;
+return_param = 0.1;
+a2 = 0.3;
+a3 = 0.7;
+
+backg_risk = 0.005;
 
 OF = 1;
 
@@ -21,7 +27,8 @@ t = M.time;
 % % (1)
 height_above_msl       =  (M.barr_elev-M.msl(t)); 
 num_sunny_day_flood    = 365*(1-height_above_msl);
-sunny_day_flood_premium = (num_sunny_day_flood^1)/a;
+%sunny_day_flood_premium = (num_sunny_day_flood^1)/a;
+sunny_day_flood_premium = a3*(1-height_above_msl)^2;
 
 X.num_sunny_day_floods_and_premium(t,:)=[num_sunny_day_flood sunny_day_flood_premium]; % just to track 
 
@@ -32,10 +39,14 @@ X.num_sunny_day_floods_and_premium(t,:)=[num_sunny_day_flood sunny_day_flood_pre
 % sunny_day_flood_premium   = num_sunny_day_flood/a;                             % convert number of sunny day floods to risk premium 
 
 % % (2)
-storm_risk_increases_with_sea_level = 1 + d / (M.barr_elev-M.msl(t));
-% dunes_reduce_storm_risk             = MMT.h_dune(t)/MMT.h0;
-dunes_reduce_storm_risk             = 1-exp(-8*(MMT.h_dune(t)/MMT.h0)^2);
-dune_premium                        = e * (storm_risk_increases_with_sea_level - dunes_reduce_storm_risk);
+%storm_risk_increases_with_sea_level = 1 + d / (M.barr_elev-M.msl(t));
+storm_risk_increases_with_sea_level = (1-M.barr_elev-M.msl(t));
+%dunes_reduce_storm_risk             = 1-exp(-8*(MMT.h_dune(t)/MMT.h0)^2);
+dunes_reduce_storm_risk             = 1-(MMT.h_dune(t)/MMT.h0);
+%dune_premium                        = e * (storm_risk_increases_with_sea_level - dunes_reduce_storm_risk);
+dune_premium                        = a2 * (storm_risk_increases_with_sea_level * dunes_reduce_storm_risk);
+
+storm_return_risk = return_interval*return_param;
 
 % % (3)
 front_row_risk = OF * f;
@@ -45,11 +56,14 @@ agent_risk_tolerance    = X.rp_base;
 investor_risk_tolerance = 1;
 
 for ii=1:X.n
-      X.rp_o(ii) = (sunny_day_flood_premium + dune_premium + front_row_risk) * agent_risk_tolerance(ii);      
+      %X.rp_o(ii) = (sunny_day_flood_premium + dune_premium + front_row_risk) * agent_risk_tolerance(ii); Old style
+      X.rp_o(ii) = backg_risk + (storm_return_risk + sunny_day_flood_premium + dune_premium + front_row_risk) * agent_risk_tolerance(ii);
 end
 X.rp_o(X.rp_o>1)=1;
 
-X.rp_I = (sunny_day_flood_premium + dune_premium + front_row_risk) * investor_risk_tolerance;
+%X.rp_I = (sunny_day_flood_premium + dune_premium + front_row_risk) * investor_risk_tolerance;
+X.rp_I = backg_risk + (storm_return_risk + sunny_day_flood_premium + dune_premium + front_row_risk) * investor_risk_tolerance;
+
 if X.rp_I > 1
     X.rp_I=1;
 end
